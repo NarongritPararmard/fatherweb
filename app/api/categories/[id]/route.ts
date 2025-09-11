dabc84e87e66e056f7bfd5ca7b9bc2ce5a802c30
NarongritPararmard <narongrit.pa@kkumail.com>
2025-09-11 23:36:43 +0700

    modify again

--- app/api/categories/[id]/route.ts
+++ app/api/categories/[id]/route.ts
@@ -2,25 +2,25 @@ import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
 
-export async function GET(
-  request: Request,
-  context: { params: Record<string, string> }  // <- ใช้ Record<string, string>
-) {
-  try {
-    const categoryId = Number(context.params.id) // <- ใช้ context.params
-    const categoryWithPosts = await prisma.category.findUnique({
-      where: { id: categoryId },
-      include: {
-        posts: true,
-      },
-    })
-    return Response.json(categoryWithPosts)
-  } catch (error) {
-    return new Response((error as Error).message, {
-      status: 500,
-    })
-  }
-}
+// export async function GET(
+//   req: Request,
+//   { params }: { params: { id: string } }
+// ) {
+//   try {
+//     const categoryId = Number(params.id)
+//     const categoryWithPosts = await prisma.category.findUnique({
+//       where: { id: categoryId },
+//       include: {
+//         posts: true, // Include related posts in the response
+//       },
+//     })
+//     return Response.json(categoryWithPosts)
+//   } catch (error) {
+//     return new Response(error as BodyInit, {
+//       status: 500,
+//     })
+//   }
+// }
 
 export async function PUT(
   req: Request,
