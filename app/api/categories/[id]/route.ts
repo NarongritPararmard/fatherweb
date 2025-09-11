cb1bcede72a17cb4ba53dafa12e990e970e94b63
NarongritPararmard <narongrit.pa@kkumail.com>
2025-09-11 23:48:48 +0700

    update

--- app/api/categories/[id]/route.ts
+++ app/api/categories/[id]/route.ts
@@ -2,34 +2,37 @@ import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
 
-// export async function GET(
-//   req: Request,
-//   { params }: { params: { id: string } }
-// ) {
-//   try {
-//     const categoryId = Number(params.id)
-//     const categoryWithPosts = await prisma.category.findUnique({
-//       where: { id: categoryId },
-//       include: {
-//         posts: true, // Include related posts in the response
-//       },
-//     })
-//     return Response.json(categoryWithPosts)
-//   } catch (error) {
-//     return new Response(error as BodyInit, {
-//       status: 500,
-//     })
-//   }
-// }
-
-export async function PUT(
-  req: Request,
+export async function GET(
+  request: Request,
   { params }: { params: { id: string } }
 ) {
   try {
-    const { name } = await req.json()
+    const { id } = await params
+    const categoryId = Number(id)
+    const categoryWithPosts = await prisma.category.findUnique({
+      where: { id: categoryId },
+      include: {
+        posts: true, // Include related posts in the response
+      },
+    })
+    return Response.json(categoryWithPosts)
+  } catch (error) {
+    return new Response(error as BodyInit, {
+      status: 500,
+    })
+  }
+}
+
+export async function PUT(
+  request: Request,
+  { params }: { params: { id: string } }
+) {
+  try {
+    const { name } = await request.json()
+    const { id } = await params
+    const categoryId = Number(id)
     const category = await prisma.category.update({
-      where: { id: Number(params.id) },
+      where: { id: categoryId },
       data: { name },
     })
     return Response.json(category)
@@ -45,9 +48,11 @@ export async function DELETE(
   { params }: { params: { id: string } }
 ) {
   try {
+    const { id } = await params
+    const categoryId = Number(id)
     return Response.json(
       await prisma.category.delete({
-        where: { id: Number(params.id) },
+        where: { id: categoryId },
       })
     )
   } catch (error) {
