43dce9546cf6e99f41631c9cdf0702181c4d32de
NarongritPararmard <narongrit.pa@kkumail.com>
2025-09-12 00:08:40 +0700

    update

--- app/api/categories/[id]/route.ts
+++ app/api/categories/[id]/route.ts
@@ -1,63 +1,63 @@
-// import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client'
 
-// const prisma = new PrismaClient()
+const prisma = new PrismaClient()
 
-// export async function GET(
-//   request: Request,
-//   { params }: { params: { id: string } }
-// ) {
-//   try {
-//     const { id } = await params
-//     const categoryId = Number(id)
-//     const categoryWithPosts = await prisma.category.findUnique({
-//       where: { id: categoryId },
-//       include: {
-//         posts: true, // Include related posts in the response
-//       },
-//     })
-//     return Response.json(categoryWithPosts)
-//   } catch (error) {
-//     return new Response(error as BodyInit, {
-//       status: 500,
-//     })
-//   }
-// }
+export async function GET(
+  request: Request,
+  { params } : { params: Promise<{ id: string }> }
+) {
+  try {
+    const { id } = await params
+    const categoryId = Number(id)
+    const categoryWithPosts = await prisma.category.findUnique({
+      where: { id: categoryId },
+      include: {
+        posts: true, // Include related posts in the response
+      },
+    })
+    return Response.json(categoryWithPosts)
+  } catch (error) {
+    return new Response(error as BodyInit, {
+      status: 500,
+    })
+  }
+}
 
-// export async function PUT(
-//   request: Request,
-//   { params }: { params: { id: string } }
-// ) {
-//   try {
-//     const { name } = await request.json()
-//     const { id } = await params
-//     const categoryId = Number(id)
-//     const category = await prisma.category.update({
-//       where: { id: categoryId },
-//       data: { name },
-//     })
-//     return Response.json(category)
-//   } catch (error) {
-//     return new Response(error as BodyInit, {
-//       status: 500,
-//     })
-//   }
-// }
+export async function PUT(
+  request: Request,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  try {
+    const { name } = await request.json()
+    const { id } = await params
+    const categoryId = Number(id)
+    const category = await prisma.category.update({
+      where: { id: categoryId },
+      data: { name },
+    })
+    return Response.json(category)
+  } catch (error) {
+    return new Response(error as BodyInit, {
+      status: 500,
+    })
+  }
+}
 
-// export async function DELETE(
-//   req: Request,
-//   { params }: { params: { id: string } }
-// ) {
-//   try {
-//     const { id } = await params
-//     const categoryId = Number(id)
-//     return Response.json(
-//       await prisma.category.delete({
-//         where: { id: categoryId },
-//       })
-//     )
-//   } catch (error) {
-//     return new Response(error as BodyInit, {
-//       status: 500,
-//     })
-//   }
-// }
+export async function DELETE(
+  req: Request,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  try {
+    const { id } = await params
+    const categoryId = Number(id)
+    return Response.json(
+      await prisma.category.delete({
+        where: { id: categoryId },
+      })
+    )
+  } catch (error) {
+    return new Response(error as BodyInit, {
+      status: 500,
+    })
+  }
+}
--- app/api/posts/[id]/route.ts
+++ app/api/posts/[id]/route.ts
@@ -4,7 +4,7 @@ const prisma = new PrismaClient()
 
 export async function GET(
     request: Request,
-    { params }: { params: { id: string } }
+    { params } : { params: Promise<{ id: string }> }
 )   {
     try {
         // const postId = Number(params.id)
@@ -24,7 +24,7 @@ export async function GET(
 
 export async function PUT(
     request: Request,
-    { params }: { params: { id: string } }
+    { params }: { params: Promise<{ id: string }> }
 )   {
     try {
         const { title, content, categoryId } = await request.json()
@@ -48,7 +48,7 @@ export async function PUT(
 
 export async function DELETE(
     request: Request,
-    { params }: { params: { id: string } }
+    { params }: { params: Promise<{ id: string }> }
 )   {
     try {
         const { id } = await params
