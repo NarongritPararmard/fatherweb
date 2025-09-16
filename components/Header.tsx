20827c02b721895303f17cde13878322facde2ff
NarongritPararmard <narongrit.pa@kkumail.com>
2025-09-16 22:39:35 +0700

    update nav bar

--- components/Header.tsx
+++ components/Header.tsx
@@ -22,7 +22,8 @@ export default function Header() {
 
   return (
     <header className="bg-gradient-to-r from-orange-500/95 to-orange-600/95 backdrop-blur-lg shadow-xl sticky top-0 z-50 border-b border-orange-600/20">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-2">
+      {/* <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-2"> */}
+      <div className="max-w-8xl px-10 py-2">
         <div className="flex justify-between items-center">
           {/* Logo Section */}
           <Link href="/" className="flex items-center space-x-5 group">
